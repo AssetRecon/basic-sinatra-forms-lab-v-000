4f88d39f22dbd5248b57fc2f38cf5ab9dabd8a8e
Edsel Caprice <caprice@mail.usf.edu>
2018-10-31 12:52:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,15 @@ erb :newteam
 end
 
 post '/team' do
+
+  @name = params['name']
+  @coach = params['coach']
+  @pg = params['pg']
+  @sg = params['sg']
+  @sf = params['sf']
+  @pf = params['pf']
+  @c = params['c']
+  
   erb :team
 end
 
